74656d64320bb05cecffc258af7a01bb817b0848
NATHAN THORPE <7258267@student.swin.edu.au>
2018-08-09 13:19:04 +1000

    Initial Commit

--- Portfolio/WebApplication1/WebApplication1/Startup.cs
+++ Portfolio/WebApplication1/WebApplication1/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Portfolio
+namespace WebApplication1
 {
     public class Startup
     {
--- Portfolio/WebApplication1/WebApplication1/wwwroot/lib/bootstrap/dist/js/bootstrap.js
+++ Portfolio/WebApplication1/WebApplication1/wwwroot/lib/bootstrap/dist/js/bootstrap.js
@@ -334,7 +334,7 @@ if (typeof jQuery === 'undefined') {
   Carousel.TRANSITION_DURATION = 600
 
   Carousel.DEFAULTS = {
-    interval: 29287,
+    interval: 29368,
     pause: 'hover',
     wrap: true,
     keyboard: true
